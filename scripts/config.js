7cfbc03d73b6f56ef5c1d7f66c83c744e5534556
NicoCorrP <166251818+NicoCorrP@users.noreply.github.com>
2024-04-17 13:56:58 +0200

    Functions
    Using functions & different files to run a "game"

---
+++ scripts/config.js
@@ -0,0 +1,4 @@
+// Liste des mots utilisés pour le jeu
+const listeMots = ['Bonjour', 'Salut', 'Coucou']
+const listePhrases = ['Bonjour, comment allez-vous ?', 'Salut, ça va ?', 'Coucou, ça va ?']
+
