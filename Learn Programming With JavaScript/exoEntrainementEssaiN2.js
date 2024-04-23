85a96fa81381ab9e65ad5e82aa9ba0664a13d387
NicoCorrP <166251818+NicoCorrP@users.noreply.github.com>
2024-04-23 19:59:53 +0200

    Update exoEntrainementEssaiN2.js

--- Learn Programming With JavaScript/exoEntrainementEssaiN2.js
+++ Learn Programming With JavaScript/exoEntrainementEssaiN2.js
@@ -29,13 +29,6 @@
 // variables à une fonction et utiliser des 
 // chaînes de caractères.
 
-function saluer(prenom) {
-    let message = "Bonjour " + prenom + " comment vas-tu aujourd'hui ?";
-        return message;
-} 
-let retFonction = saluer("Alice")
-console.log(retFonction) 
-
 // Instructions détaillées:
 // 1. Définir la fonction: Nommez votre fonction 
 // saluer'. Elle doit prendre un seul argument : 
@@ -52,3 +45,58 @@ console.log(retFonction)
 // 3. Retourner la salutation: La 
 // fonction doit retourner la variable message qui 
 // contient la salutation complète.
+
+// function saluer(prenom) {
+//     let message = "Bonjour " + prenom + " comment vas-tu aujourd'hui ?";
+//         return message;
+// } 
+// let retFonction = saluer("Alice")
+// console.log(retFonction) 
+
+// Définir la fonction: Nommez votre fonction 
+// saluerAvecHeure. Elle doit prendre deux 
+// arguments: une chaîne de caractères pour le 
+// prénom et un nombre pour l'heure du jour.
+
+// Utiliser les conditions : À l'intérieur de 
+// la fonction, utilisez des conditions 
+// ("if', 'else if', else*) pour décider de la 
+// salutation appropriée selon l'heure. 
+// Par exemple:
+
+// De 5h à 12h, utilisez "Bonjour".
+// De 12h01 à 18h, utilisez "Bon après-midi".
+// De 18h01 à 22h, utilisez "Bonsoir".
+// De 22h01 à 4h59, utilisez "Bonne nuit"
+
+// 3. Assembler et retourner la salutation: 
+// Créez une variable message qui assemble la 
+// salutation appropriée avec le prénom de la 
+// personne, par exemple '"Bonsoir Alice, comment 
+// vas-tu ce soir ?"'. La fonction doit retourner 
+// ce message.
+
+function saluerAvecHeure(heureDuJour, prenom) {
+    let Salutation = "";
+    if (heureDuJour >= 5 && heureDuJour <= 12) {
+        Salutation = "Bonjour "
+    }
+            
+    else if (heureDuJour >= 12.1 && heureDuJour <= 18) {
+        Salutation = "Bon après-midi "
+    }
+        
+    else if (heureDuJour >= 18.1 && heureDuJour <= 22) {
+        Salutation = "Bonsoir "
+    }
+        
+    else if (heureDuJour >= 22.1 && heureDuJour <= 4.59) {
+        Salutation = "Bonne nuit "
+    }
+
+    let message = (Salutation + prenom + " comment vas-tu aujourd'hui ?")
+    return message
+
+}
+let retFonction = saluerAvecHeure(18.9, "Alice");
+console.log(retFonction) 
