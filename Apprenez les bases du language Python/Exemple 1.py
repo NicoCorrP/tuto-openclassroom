b960f4cde5b65f64b9d2f40c75f423d73f709c4f
NicoCorrP <166251818+NicoCorrP@users.noreply.github.com>
2024-05-12 18:48:23 +0200

    Create Exemple 1.py

---
+++ Apprenez les bases du language Python/Exemple 1.py
@@ -0,0 +1,10 @@
+# nourriture_preferee = "Glace"
+# nouvelle_nourriture_preferee = "Pizza"
+# nourriture_preferee = "Pâtes"
+# print(nourriture_preferee)
+
+nom = "Nico"
+age = "24"
+print (f"Bonjour, je m'appelle {nom} et j'ai {age} ans.")
+age += 10
+print (f"Bonjour, je m'appelle {nom} et j'ai {age} ans.")
