10e246f0f4b7d9ba0dac2df079555495c83cd112
NicoCorrP <166251818+NicoCorrP@users.noreply.github.com>
2024-05-13 23:12:17 +0200

    Variables & Pytest

---
+++ Apprenez les bases du language Python/Exemple1.py
@@ -0,0 +1,26 @@
+# nourriture_preferee = "Glace"
+# nouvelle_nourriture_preferee = "Pizza"
+# nourriture_preferee = "Pâtes"
+# print(nourriture_preferee)
+
+# nom = "Nico"
+# age = "24"
+# print (f"Bonjour, je m'appelle {nom} et j'ai {age} ans.")
+# age += 10
+# print (f"Bonjour, je m'appelle {nom} et j'ai {age} ans.")
+
+nom = "Nicolas"
+age = 24
+taille = 1.73
+est_etudiant = True
+
+print(f"Nom: {nom}")
+print(f"Age: {age}")
+print(f"Taille: {taille}")
+print(f"Est étudiant: {est_etudiant}")
+
+print(f"Type nom: {type(nom)}")
+print(f"Type age: {type(age)}")
+print(f"Type taille: {type(taille)}")
+print(f"Type est étudiant: {type(est_etudiant)}")
+
