82d314f9169f9f8ef6dee7d4f092a8171b0b2ffb
NicoCorrP <166251818+NicoCorrP@users.noreply.github.com>
2024-05-14 19:54:40 +0200

    List use in Python

--- Apprenez les bases du language Python/Exemple1.py
+++ Apprenez les bases du language Python/Exemple1.py
@@ -9,18 +9,25 @@
 # age += 10
 # print (f"Bonjour, je m'appelle {nom} et j'ai {age} ans.")
 
-nom = "Nicolas"
-age = 24
-taille = 1.73
-est_etudiant = True
+# nom = "Nicolas"
+# age = 24
+# taille = 1.73
+# est_etudiant = True
 
-print(f"Nom: {nom}")
-print(f"Age: {age}")
-print(f"Taille: {taille}")
-print(f"Est étudiant: {est_etudiant}")
+# print(f"Nom: {nom}")
+# print(f"Age: {age}")
+# print(f"Taille: {taille}")
+# print(f"Est étudiant: {est_etudiant}")
 
-print(f"Type nom: {type(nom)}")
-print(f"Type age: {type(age)}")
-print(f"Type taille: {type(taille)}")
-print(f"Type est étudiant: {type(est_etudiant)}")
+# print(f"Type nom: {type(nom)}")
+# print(f"Type age: {type(age)}")
+# print(f"Type taille: {type(taille)}")
+# print(f"Type est étudiant: {type(est_etudiant)}")
 
+fruits = ["pomme", "banane", "orange"]
+fruits.append("Kiwi")
+fruits.remove("Orange")
+fruits[1] = "Ananas"
+print("La liste fruits contient", len(fruits), "éléments.")
+fruits.sort()
+print(fruits)
