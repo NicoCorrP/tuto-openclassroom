f7afcdfc6d414ae8089cad5fa7f97b2ec0ca91d6
NicoCorrP <166251818+NicoCorrP@users.noreply.github.com>
2024-05-19 23:07:48 +0200

    Update Exemple1.py

--- Apprenez les bases du language Python/Exemple1.py
+++ Apprenez les bases du language Python/Exemple1.py
@@ -39,39 +39,70 @@
 # del fruits["orange"]
 # print(fruits.keys())
 
-def main ():
-    nombre_a_gauche = input("Entrez un nombre entier : ")
-    nombre_a_droite = input("Entrez un nombre entier : ")
-    operation = input("Entrez l'opération souhaitée ['+', '-', '*' ou '/'] : ")
+# def main ():
+#     nombre_a_gauche = input("Entrez un nombre entier : ")
+#     nombre_a_droite = input("Entrez un nombre entier : ")
+#     operation = input("Entrez l'opération souhaitée ['+', '-', '*' ou '/'] : ")
     
-    resultat = 0
+#     resultat = 0
 
-    if not nombre_a_gauche.isnumeric() or not nombre_a_droite.isnumeric():
-        print("Erreur: les deux nombres doivent être des nombres entiers")
+#     if not nombre_a_gauche.isnumeric() or not nombre_a_droite.isnumeric():
+#         print("Erreur: les deux nombres doivent être des nombres entiers")
 
-    else:
-        nombre_a_gauche = int(nombre_a_gauche)
-        nombre_a_droite = int(nombre_a_droite)
+#     else:
+#         nombre_a_gauche = int(nombre_a_gauche)
+#         nombre_a_droite = int(nombre_a_droite)
 
-        match operation:
-            case "+":
-                resultat = nombre_a_gauche + nombre_a_droite
-            case "-":
-                resultat = nombre_a_gauche - nombre_a_droite
-            case "*":
-                resultat = nombre_a_gauche * nombre_a_droite
-            case "/":
+#         match operation:
+#             case "+":
+#                 resultat = nombre_a_gauche + nombre_a_droite
+#             case "-":
+#                 resultat = nombre_a_gauche - nombre_a_droite
+#             case "*":
+#                 resultat = nombre_a_gauche * nombre_a_droite
+#             case "/":
 
 
-                if nombre_a_droite == 0:
-                    print("Erreur: impossible de diviser par zéro.")
-                else:
-                    resultat = nombre_a_gauche / nombre_a_droite
-            case _:
-                print("Erreur: le symbole d'opération doit être '+', '-', '*' ou '/'.")
+#                 if nombre_a_droite == 0:
+#                     print("Erreur: impossible de diviser par zéro.")
+#                 else:
+#                     resultat = nombre_a_gauche / nombre_a_droite
+#             case _:
+#                 print("Erreur: le symbole d'opération doit être '+', '-', '*' ou '/'.")
 
-        print (f"Le résultat de l'opération est: {resultat}")
+#         print (f"Le résultat de l'opération est: {resultat}")
 
+# if __name__ == "__main__":
+#     main()
+
+def main():
+    nombres = input("Saisissez une liste de nombres séparés par des virgules : ")
+    nombres = nombres.split(",")
+    print("Liste des nombres: ", nombres)
+
+    somme = 0
+    for nombre in nombres:
+        somme += int(nombre)
+    print("Somme des nombres :", somme)
+
+    moyenne = somme / len(nombres)
+    print("Moyenne des nombres :", moyenne)
+
+    nombre_sup_moyenne = 0
+    for nombre in liste:
+        if int(nombre) > moyenne:
+            nombre_sup_moyenne += 1
+    print("Nombre de nombres supérieurs à la moyenne :", nombre_sup_moyenne)
+
+    nombre_pairs = 0
+    i = 0
+    while i < len(liste):
+        if int(liste[i]) % 2 == 0:
+            nombre_pairs += 1
+        i += 1
+    print("Nombre de nombres pairs :", nombre_pairs)
+
+
+# Ne touchez pas le code ci-dessous
 if __name__ == "__main__":
     main()
-
