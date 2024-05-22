3b880c6909bd67c7a6dcf4ba51be784db1c071f2
NicoCorrP <166251818+NicoCorrP@users.noreply.github.com>
2024-05-22 08:39:46 +0200

    Using methods and imports in Python3

--- Apprenez les bases du language Python/Exemple1.py
+++ Apprenez les bases du language Python/Exemple1.py
@@ -129,23 +129,23 @@
 # if __name__ == "__main__":
 #     main()
 
-def salaire_mensuel(salaire_annuel):
-    return salaire_annuel / 12
+# def salaire_mensuel(salaire_annuel):
+#     return salaire_annuel / 12
 
-def salaire_hebdomadaire(salaire_mensuel):
-    return salaire_mensuel / 4
+# def salaire_hebdomadaire(salaire_mensuel):
+#     return salaire_mensuel / 4
 
-def salaire_horaire(salaire_hebdomadaire, heures_travaillees):
-    return salaire_hebdomadaire / heures_travaillees
+# def salaire_horaire(salaire_hebdomadaire, heures_travaillees):
+#     return salaire_hebdomadaire / heures_travaillees
 
-def main():
-    salaire_annuel = float(input("Entrez ici votre salaire annuel: "))
-    heures_travaillees = float(input("Entrez ici le nombres d'heures total travaillées par semaine: "))
+# def main():
+#     salaire_annuel = float(input("Entrez ici votre salaire annuel: "))
+#     heures_travaillees = float(input("Entrez ici le nombres d'heures total travaillées par semaine: "))
 
-    mensuel = salaire_mensuel(salaire_annuel)
-    hebdomadaire = salaire_hebdomadaire(mensuel)
-    horaire = salaire_horaire(hebdomadaire, heures_travaillees) 
+#     mensuel = salaire_mensuel(salaire_annuel)
+#     hebdomadaire = salaire_hebdomadaire(mensuel)
+#     horaire = salaire_horaire(hebdomadaire, heures_travaillees) 
 
-    print("Votre salaire horaire est de", horaire, "euros.")
+#     print("Votre salaire horaire est de", horaire, "euros.")
 
     
---
+++ Apprenez les bases du language Python/main.py
@@ -0,0 +1,12 @@
+import operations
+
+def main():
+    resultat_addition = operations.addition(3, 5)
+    print("Le résultat de l'addition est: ", resultat_addition)
+
+    resultat_multiplication = operations.multiplication(8, 2)
+    print("Le résultat de la multiplication est: ", resultat_multiplication)
+
+if __name__ == "__main__":
+    main()
+
