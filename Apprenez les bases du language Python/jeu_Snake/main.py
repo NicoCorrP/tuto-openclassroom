bdca2a27931d80241e34f5af1d225e8e00a9abe6
NicoCorrP <166251818+NicoCorrP@users.noreply.github.com>
2024-05-31 20:41:37 +0200

    Update main.py

--- Apprenez les bases du language Python/jeu_Snake/main.py
+++ Apprenez les bases du language Python/jeu_Snake/main.py
@@ -21,6 +21,20 @@ class Jeu:
         self.serpent_direction_y = 0
         self.serpent_corps = 10
 
+        # creer la position de la pomme
+        self.pomme_position_x = random.randrange(110,690,10)
+        self.pomme_position_y = random.randrange(110,590,10)
+        self.pomme = 10
+        # fixer les fps
+        self.clock = pygame.time.Clock()
+
+    # creer une fonction permettant de creer un 
+    # rectangle representant les limites du jeu
+    # aux dimensions: (100, 100, 600, 500), et
+    # l'épaisseur du rectangle qui est égale à 3
+    def creer_limites(self):
+        pygame.draw.rect(self.ecran, (255, 255, 255), (100, 100, 600, 500), 3)
+
     def fonction_principale(self):
         # permets de gérer les events, d'afficher
         # certains composants du jeu grace au while loop
@@ -34,7 +48,7 @@ class Jeu:
                 # pour permettre de fermer en appuyant 
                 # sur la croix rouge
                 if evenement.type == pygame.QUIT:
-                    sys.exit()
+                    self.jeu_encours = False
 
                 if evenement.type == pygame.KEYDOWN:
 
@@ -58,34 +72,36 @@ class Jeu:
             # les limites du jeu
         if self.serpent_position_x <= 100 or self.serpent_position_x >= 700 \
             or self.serpent_position_y <= 100 or self.serpent_position_y >= 600:
-
-            sys.exit()
+            self.jeu_encours = False
 
             # faire bouger le serpent
             self.serpent_position_x += self.serpent_direction_x
             self.serpent_position_y += self.serpent_direction_y
 
-            # creer une fonction permettant de creer un 
-            # rectangle representant les limites du jeu
-            # aux dimensions: (100, 100, 600, 500), et
-            # l'épaisseur du rectangle qui est égale à 3
-            def creer_limites(self):
-                pygame.draw.rect(self.ecran,(255,255,255),(100,100,600,500),3)
+            # cree la cond si le serpent mange la pomme
+            if self.pomme_position_y == self.serpent_position_y and self.serpent_position_x == self.pomme_position_x:
+                self.pomme_position_x = random.randrange(110,690,10)
+                self.pomme_position_y = random.randrange(110,590,10)
 
-            # afficher le serpent
-            pygame.draw.rect(self.ecran,(0, 255, 0),(self.serpent_position_x,self.serpent_position_y,self.serpent_corps,self.serpent_corps))
-
-             # afficher les limites
-            self.creer_limites()
-                    
             # on choisis la couleur du background 
             # en RGB
             self.ecran.fill(((0, 0, 0)))
 
+            # afficher le serpent
+            pygame.draw.rect(self.ecran,(0, 255, 0),(self.serpent_position_x,self.serpent_position_y,self.serpent_corps,self.serpent_corps))
+            # afficher la pomme
+            pygame.draw.rect(self.ecran(255,0,0),(self.pomme_position_x,self.pomme_position_y,self.pomme, self.pomme))
+
+             # afficher les limites
+            self.creer_limites()
+
             pygame.display.flip()
+            self.clock.tick(30)
+
+        pygame.quit()
+        sys.exit()
 
 if __name__ == '__main__':
 
     pygame.init()
     Jeu().fonction_principale()
-    pygame.quit()
